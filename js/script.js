0ae182a0c0fc74af9735fe129d5a0c0aa7aada2c
Ryan Yang <javathon@gmail.com>
2018-11-21 16:16:11 -0800

    instead of using webgl straight up, I'll probably use plotly.js instead

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 function load(){
-    console.log('Start');
+    console.log("started");
 }
+Plotly.d3.csv( 'https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv', function ( err, rows ) {
+    function unpack( rows, key ) {
+        return rows.map( function ( row ) {
+            return row[ key ];
+        } );
+    }
+    var z_data = []
+    for ( i = 0; i < 24; i++ ) {
+        z_data.push( unpack( rows, i ) );
+    }
+    var data = [ {
+        z: z_data,
+        type: 'surface'
+    } ];
+
+    var layout = {
+        title: 'Mt Bruno Elevation',
+        autosize: false,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        margin: {
+            l: 65,
+            r: 50,
+            b: 65,
+            t: 90,
+        }
+    };
+    Plotly.newPlot( 'graph', data, layout );
+} );

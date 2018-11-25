c77f19a542edc421ae4b880f02d485012140c269
Ryan Yang <javathon@gmail.com>
2018-11-24 19:21:42 -0800

    adding more functionality to graphs themselves

--- js/graph.js
+++ js/graph.js
@@ -1,35 +1,64 @@
-function graph(equation){
+function equation(x, y){
+    return Math.cos(x) * Math.sin(y);
+}
+function calculateEquationReturnData(equation, limits){
     let x = [], y = [], z = [];
-    for ( let xCoordinate = 0; xCoordinate < 10; xCoordinate++ ) {
+    let dimensions = {
+        xS: limits.x1,
+        xL: limits.x2,
+        yS: limits.y1,
+        yL: limits.y2,
+        zS: 0,
+        zL: 0,
+    };
+    for ( let xCoordinate = limits.x1; xCoordinate < limits.x2; xCoordinate++ ) {
         x[xCoordinate] = [];
         y[xCoordinate] = [];
         z[xCoordinate] = [];
-        for( let yCoordinate = 0; yCoordinate < 10; yCoordinate++ ){
+        for( let yCoordinate = limits.y1; yCoordinate < limits.y2; yCoordinate++ ){
+            let zCoordinate = equation(xCoordinate, yCoordinate);
+            if(zCoordinate > zL)
             x[xCoordinate].push(xCoordinate);
             y[xCoordinate].push(yCoordinate);
-            z[xCoordinate].push(xCoordinate*xCoordinate+yCoordinate);
-            console.log(`(${x[xCoordinate][yCoordinate]}, ${y[xCoordinate][yCoordinate]}, ${z[xCoordinate][yCoordinate]})`);
+            z[xCoordinate].push(zCoordinate);
         }
     }
-    z.forEach(e =>{
-        console.log(e);
-    })
+    graph(z, dimensions);
+}
+function graph(z, dimensions){
     var data = [{
         z: z,
         type: 'surface', 
         showscale: false
     }];
     var layout = {
-        autosize: true,
+        autosize: false,
         width: window.innerWidth,
         height: window.innerHeight,
         margin: {
-            l: 80,
-            r: 80,
-            b: 80,
-            t: 20,
+            l: 0,
+            r: 0,
+            b: 0,
+            t: 0,
         },
-        fill: "#444",
+        scene: {
+            aspectmode: "manual",
+            aspectratio: {
+                x: 2,
+                y: 2,
+                z: 2,
+            },
+            xaxis: {
+                color: '#000',
+                range: [ dimensions.xS, dimensions.xL ],
+            },
+            yaxis: {
+                range: [ dimensions.yS, dimensions.yL ],
+            },
+            zaxis: {
+                range: [ dimensions.zS, dimensions.zL ],
+            }
+        }
     };
     Plotly.newPlot( 'graph', data, layout );
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-
-
 function load(){
-    graph();
+    calculateEquationReturnData(equation, {
+        x1 : -100,
+        x2 : 100,
+        y1 : -100,
+        y2 : 100,
+    });
 }
-
